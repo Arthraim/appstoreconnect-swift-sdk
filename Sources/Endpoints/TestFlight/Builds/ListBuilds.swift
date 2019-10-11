f85f9a43a3eff5040f0b5f86f3cdf32a096a7975
Arthur Wang <arthraim@gmail.com>
2019-10-11 20:37:32 +0800

    Fix ListBuilds.Field.BuildBetaDetails

--- Sources/Endpoints/TestFlight/Builds/ListBuilds.swift
+++ Sources/Endpoints/TestFlight/Builds/ListBuilds.swift
@@ -46,10 +46,10 @@ public struct ListBuilds {
         case betaTesters([BetaTester])
         case builds([Build])
         case preReleaseVersions([PreReleaseVersion])
-        case buildBetaDetails([BuildBetaDetail])
+        case buildBetaDetails([BuildBetaDetails])
         case betaAppReviewSubmissions([BetaAppReviewSubmission])
         case betaBuildLocalizations([BetaBuildLocalization])
-        
+
         static var key: String = "fields"
         var pair: Pair {
             switch self {
@@ -64,7 +64,7 @@ public struct ListBuilds {
             case .preReleaseVersions(let value):
                 return (PreReleaseVersion.key, value.map({ $0.pair.value }).joinedByCommas())
             case .buildBetaDetails(let value):
-                return (BuildBetaDetail.key, value.map({ $0.pair.value }).joinedByCommas())
+                return (BuildBetaDetails.key, value.map({ $0.pair.value }).joinedByCommas())
             case .betaAppReviewSubmissions(let value):
                 return (BetaAppReviewSubmission.key, value.map({ $0.pair.value }).joinedByCommas())
             case .betaBuildLocalizations(let value):
@@ -72,7 +72,7 @@ public struct ListBuilds {
             }
         }
     }
-    
+
     /// Attributes, relationships, and IDs by which to filter.
     public enum Filter: NestableQueryParameter {
         case app([String])
@@ -112,7 +112,7 @@ public struct ListBuilds {
             }
         }
     }
-    
+
     /// Relationship data to include in the response.
     public enum Include: String, CaseIterable, NestableQueryParameter {
         case app, appEncryptionDeclaration, betaAppReviewSubmission, betaBuildLocalizations, buildBetaDetail, individualTesters, preReleaseVersion
@@ -120,7 +120,7 @@ public struct ListBuilds {
         static var key: String = "include"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     /// Number of resources to return.
     public enum Limit: NestableQueryParameter {
         case individualTesters(Int), betaBuildLocalizations(Int)
@@ -135,7 +135,7 @@ public struct ListBuilds {
             }
         }
     }
-    
+
     /// Attributes by which to sort.
     public enum Sort: String, CaseIterable, NestableQueryParameter {
         case preReleaseVersionAscending = "+preReleaseVersion"
@@ -152,56 +152,56 @@ public struct ListBuilds {
 
 // MARK: - Field
 extension ListBuilds.Field {
-    
+
     public enum AppEncryptionDeclaration: String, CaseIterable, NestableQueryParameter {
         case app, appEncryptionDeclarationState, availableOnFrenchStore, builds, codeValue, containsProprietaryCryptography, containsThirdPartyCryptography, documentName, documentType, documentUrl, exempt, platform, uploadedDate, usesEncryption
 
         static var key: String = "AppEncryptionDeclaration"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum App: String, CaseIterable, NestableQueryParameter {
         case betaAppLocalizations, betaAppReviewDetail, betaGroups, betaLicenseAgreement, betaTesters, builds, bundleId, name, preReleaseVersions, primaryLocale, sku
 
         static var key: String = "App"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum BetaTester: String, CaseIterable, NestableQueryParameter {
         case apps, betaGroups, builds, email, firstName, inviteType, lastName
 
         static var key: String = "betaTester"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum Build: String, CaseIterable, NestableQueryParameter {
         case app, appEncryptionDeclaration, betaAppReviewSubmission, betaBuildLocalizations, betaGroups, buildBetaDetail, expirationDate, expired, iconAssetToken, individualTesters, minOsVersion, preReleaseVersion, processingState, uploadedDate, usesNonExemptEncryption, version
 
         static var key: String = "build"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum PreReleaseVersion: String, CaseIterable, NestableQueryParameter {
         case app, builds, platform, version
 
         static var key: String = "preReleaseVersion"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
-    public enum BuildBetaDetail: String, CaseIterable, NestableQueryParameter {
+
+    public enum BuildBetaDetails: String, CaseIterable, NestableQueryParameter {
         case autoNotifyEnabled, build, externalBuildState, internalBuildState
 
-        static var key: String = "buildBetaDetail"
+        static var key: String = "buildBetaDetails"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum BetaAppReviewSubmission: String, CaseIterable, NestableQueryParameter {
         case betaReviewState, build
 
         static var key: String = "betaAppReviewSubmission"
         var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
     }
-    
+
     public enum BetaBuildLocalization: String, CaseIterable, NestableQueryParameter {
         case build, locale, whatsNew
 
@@ -212,7 +212,7 @@ extension ListBuilds.Field {
 
 // MARK: - Filter
 extension ListBuilds.Filter {
-    
+
     public enum ProcessingState: String, CaseIterable, NestableQueryParameter {
         case PROCESSING, FAILED, INVALID, VALID
 
